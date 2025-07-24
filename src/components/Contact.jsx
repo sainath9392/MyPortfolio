80f74855938a2679cf7e143b29ca08e635931ff8
sainath9392 <sainathduvvuri03@gmail.com>
2025-07-24 21:15:03 +0530

    contact backgroud

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div id='contact' className="h-[100vh] w-[100vw] bg-[url(/img/background.png)] bg-center bg-no-repeat  overflow-hidden">
-      
+    <div id="contact" className="h-[100vh] w-[100vw] overflow-hidden relative">
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover z-0"
+      >
+        <source src="/videos/back.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div class="absolute inset-0 bg-black/40 backdrop-blur-sm z-10"></div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
